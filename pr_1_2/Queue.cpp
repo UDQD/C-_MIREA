13e15fd893aeb3698696d98d7ff175c4d4830d06
UDQD <70804333+UDQD@users.noreply.github.com>
2020-10-06 10:35:34 +0300

    v1.0.0

--- pr_1_2/Queue.cpp
+++ pr_1_2/Queue.cpp
@@ -1,71 +1,58 @@
-//
-// Created by UDQD on 21.09.2020.
-//
 #include <iostream>
+
 using namespace std;
 
 #include "Queue.h"
 
-Queue::~Queue()
-{
-    Node* temp = Head;
-    while (temp != NULL)
-
-    {
+Queue::~Queue() {
+    Node *temp = Head;
+    while (temp != NULL) {
         temp = Head->Next;
         delete Head;
         Head = temp;
 
     }
 }
-void Queue::Add_elem(string x)
-{
-    Node* temp = new Node;
+
+void Queue::Add_elem(string x) {
+    Node *temp = new Node;
     temp->x = x;
     temp->Next = NULL;
-    cout << "Elem ";
-    cout << x;
-    cout << " was added in \n";
+//    cout << "Elem ";
+//    cout << x;
+//    cout << " was added\n";
 
-    if (Head != NULL)
-    {
+    if (Head != NULL) {
         Tail->Next = temp;
         Tail = temp;
-    }
-    else Head = Tail = temp;
+    } else Head = Tail = temp;
 }
 
-string Queue::del()
-{
-    if (Head != NULL)
-    {
-        Node* temp = Head;
+void Queue::del() {
+    if (Head != NULL) {
+        Node *temp = Head;
 //        cout << "Elem " << Head->x << " out " << endl;
         Head = Head->Next;
-        return temp->x;
+//        return temp->x;
         delete temp;
 
-    }
-    else cout << "queue is empty \n";
+    } else cout << "queue is empty \n";
 }
-void Queue::isempty() {
-    Node* temp = Head;
-    if (temp == NULL)	cout << "queue is empty \n";
-    else cout << "queue is not empty \n";
-}
+
 string Queue::Show() {
-    Node* temp = Head;
+    Node *temp = Head;
     return temp->x;
 //    if (temp == NULL)	cout << "queue is empty \n";
 //    else cout << "Elem " << temp->x << " - first in queue " << "\n";
 }
+
 int Queue::len() {
-    Node* temp = Head;
+    Node *temp = Head;
     int c = 0;
     if (temp == NULL) return 0;
-    else{
-        while(temp!=NULL){
-            temp=temp->Next;
+    else {
+        while (temp != NULL) {
+            temp = temp->Next;
             c++;
         }
         return c;
--- pr_1_2/Queue.h
+++ pr_1_2/Queue.h
@@ -1,29 +1,28 @@
-//
-// Created by UDQD on 21.09.2020.
-//
 #include <iostream>
 
 #ifndef PR_1_2_QUEUE_H
 #define PR_1_2_QUEUE_H
 
-struct Node
-{
+struct Node {
     std::string x;
-    Node* Next;
+    Node *Next = NULL;
 };
 
 class Queue {
 
-        Node *Head, *Tail;
-        public:
-        Queue() :Head(NULL), Tail(NULL)
-        {};
-        ~Queue();
-        void Add_elem(std::string x);
-        string Show();
-        string del();
-        void isempty();
-        int len();
+    Node *Head, *Tail;
+public:
+    Queue() : Head(NULL), Tail(NULL) {};
+
+    ~Queue();
+
+    void Add_elem(std::string x);
+
+    string Show();
+
+    void del();
+
+    int len();
 
 
 };
